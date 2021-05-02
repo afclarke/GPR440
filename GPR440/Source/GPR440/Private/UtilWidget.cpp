1edd26fa11f8331315d720a1fb622493d282eb50
Adam Clarke <adam.clarke@mymail.champlain.edu>
2021-05-02 17:24:04 -0500

    utility curve drawing

---
+++ GPR440/Source/GPR440/Private/UtilWidget.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "UtilWidget.h"
+
+
+//int32 UUtilWidget::NativePaint(const FPaintArgs& Args, const FGeometry& AllottedGeometry,
+//	const FSlateRect& MyCullingRect, FSlateWindowElementList& OutDrawElements, int32 LayerId,
+//	const FWidgetStyle& InWidgetStyle, bool bParentEnabled) const
+//{
+//	draw
+//}
---
+++ GPR440/Source/GPR440/Public/UtilWidget.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "UtilWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GPR440_API UUtilWidget : public UUserWidget
+{
+	GENERATED_BODY()
+
+	//int32 NativePaint(const FPaintArgs& Args, const FGeometry& AllottedGeometry,
+	//	const FSlateRect& MyCullingRect, FSlateWindowElementList& OutDrawElements, int32 LayerId,
+	//	const FWidgetStyle& InWidgetStyle, bool bParentEnabled) const override;
+};
